5a978aa85c02e5f6284b94232b0a72e88596fb17
Georgi Sundberg <44953283+Chugarah@users.noreply.github.com>
2025-01-05 14:56:18 +0100

    Added a simple Readme

---
+++ Nexus/Program.cs
@@ -0,0 +1,43 @@
+﻿using Core.Factories;
+using Core.Helpers;
+using Core.Interface;
+using Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Nexus.Interfaces;
+using Nexus.Views;
+
+namespace Nexus;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        // Setting Upp Dependency Injection DI
+        // https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection-usage
+        // https://www.c-sharpcorner.com/article/using-dependency-injection-in-net-console-apps/
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        // Let's build the service Provider
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Now we can get the HomeView and run i
+        var homeView = serviceProvider.GetRequiredService<IHomeView>();
+        homeView.Run();
+    }
+    // https://stackoverflow.com/questions/32459670/resolving-instances-with-asp-net-core-di-from-within-configureservices
+    // IServiceCollection interface is used for building our dependency injection container
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // Registering we are using AddSingleton when we need a single state
+        // We want to maintain our state a cross our application
+        services.AddSingleton<IHomeView, HomeView>();
+        services.AddSingleton<IMenuFactory, MenuFactory>();
+        services.AddSingleton<IIdHelpers, IdHelpers>();
+
+        // Registering the MenuService
+        services.AddSingleton<IMenuCreateService, MenuService>();
+        services.AddSingleton<IGetMenuService, MenuService>();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello World
+
+Hello World :)
+
+The world is flat!
